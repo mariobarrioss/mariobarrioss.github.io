e3b318404a2baa8a88fd8c179d9c786dd658decf
Mario Barrios <mariobarriossalazar@outlook.com>
2020-08-21 10:00:09 -0600

    Add change image functionality to gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,15 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.alt
+
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+    });
+  });
 };
 
